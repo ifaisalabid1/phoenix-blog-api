7a9ea0616830f689de96f2b36f13221c71d57b2d
Faisal Abid <ifaisalabid1@gmail.com>
2025-07-05 16:01:35 +0530

    Add cors

--- lib/my_blog_web/endpoint.ex
+++ lib/my_blog_web/endpoint.ex
@@ -1,6 +1,8 @@
 defmodule MyBlogWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :my_blog
 
+  plug CORSPlug
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\my_blog" eval MyBlog.Release.migrate
